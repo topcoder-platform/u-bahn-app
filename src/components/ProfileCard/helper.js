75b28c657135e0de42a1a224d1d2ba8a2550072d
Mithun Kamath <callmekatootie@gmail.com>
2020-06-25 16:26:11 +0530

    Fix issue where adding a new skill to a user was not working

--- src/components/ProfileCard/helper.js
+++ src/components/ProfileCard/helper.js
@@ -19,7 +19,7 @@ async function checkIfSkillExists(apiClient, skillProviderId, skillExternalId) {
     // TODO - handle error
   }
 
-  if (response.data && response.data[0].id) {
+  if (response.data) {
     return response.data;
   }
 
