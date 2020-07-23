b0dda13787b128c286e2a648f2bd623fc7f386bd
Romit Choudhary <romit.choudhary@rivigo.com>
2020-07-23 17:21:36 +0530

    fixes issue#305

--- client/src/components/Input/index.jsx
+++ client/src/components/Input/index.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import PT from "prop-types";
+import _ from "lodash";
 
 import style from "./style.module.scss";
 
 export default function Input({ className, label, onChange, value }) {
   let containerStyle = style.container;
   if (className) containerStyle += ` ${className}`;
+  const id = _.uniqueId("input_");
 
   return (
     <div className={containerStyle}>
-      <div className={style.label}>{label}</div>
-      <input className={style.input} onChange={onChange} value={value} />
+      <label htmlFor={id} className={style.label}>
+        {label}
+      </label>
+      <input
+        id={id}
+        className={style.input}
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
 }
