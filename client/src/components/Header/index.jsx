4d5f01003609c58c870c7a0d5a968cfa416524f6
Mithun Kamath <callmekatootie@gmail.com>
2020-07-26 13:29:14 +0530

    Revert "issue 507 fix"

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PT from "prop-types";
 
-import { useSearch } from "../../lib/search";
 import { ReactComponent as SearchTabIcon } from "../../assets/images/search-tab-icon.svg";
 import { ReactComponent as GroupsTabIcon } from "../../assets/images/groups-tab-icon.svg";
 import { ReactComponent as UploadsTabIcon } from "../../assets/images/uploads-tab-icon.svg";
@@ -26,7 +25,6 @@ export default function Header({
   onTabChange,
   organization,
 }) {
-  const searchContext = useSearch();
   const [searchText, setSearchText] = React.useState("");
   const [showAccountDropdown, setShowAccountDropdown] = React.useState(false);
 
@@ -44,7 +42,6 @@ export default function Header({
       alert("Enter talent or keyword to search");
       return;
     }
-    searchContext.changePageNumber(1);
     onSearch && onSearch(value.trim());
   };
 
