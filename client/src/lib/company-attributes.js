55478b91651741aeeb57aab96ac87b2dcb82e4b5
edison-nguyen <edison.nguyen@linecorp.com>
2020-07-28 17:51:24 +0700

    changes for #15

--- client/src/lib/company-attributes.js
+++ client/src/lib/company-attributes.js
@@ -3,13 +3,6 @@ import config from "../config";
 import * as OrgService from "../services/user-org";
 import Axios from "axios";
 
-let primaryAttributeIds = [
-  config.STANDARD_USER_ATTRIBUTES.location,
-  config.STANDARD_USER_ATTRIBUTES.isAvailable,
-  config.STANDARD_USER_ATTRIBUTES.title,
-  config.STANDARD_USER_ATTRIBUTES.company,
-];
-
 /**
  * Get the attributes associated with the company (organization)
  * @param {Object} apiClient The api client (you can get this from src/services/api and then call api() to get the apiClient)
@@ -69,15 +62,6 @@ export async function getCompanyAttributes(apiClient, cancelToken) {
       attributes = attributes.concat(response.data);
     }
   }
-
-  // Finally, we only need the company attributes
-  attributes = attributes.filter((attribute) => {
-    if (primaryAttributeIds.includes(attribute.name)) {
-      return false;
-    }
-
-    return true;
-  });
 
   return attributes;
 }
